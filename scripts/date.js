fae1fe4fb41dd8f185abf6bfc8315d84acf0c6a8
gabriela-tomazzi <gabtomazzi@gmail.com>
2021-03-08 17:28:29 -0300

    small changes

--- scripts/date.js
+++ scripts/date.js
@@ -1,8 +1,8 @@
-var date = new Date;
 const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
 let div_hora = document.getElementById('div_hora');
 
 setInterval(()=> {
+	var date = new Date;
 	var hora = date.getHours();
 	var min = date.getMinutes();
 	var day = date.getDate();
@@ -16,4 +16,5 @@ setInterval(()=> {
 	}
 	
 	div_hora.innerHTML = `${month_full} ${day}, ${hora}:${min}`;
+	console.log('tick');
 }, 1000)
