48fa9de754259c23af97fa93a8d48eb506e622c3
gabriela-tomazzi <gabtomazzi@gmail.com>
2021-03-08 16:41:45 -0300

    switche swith for loop

--- scripts/date.js
+++ scripts/date.js
@@ -1,5 +1,5 @@
 var date = new Date;
-
+const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
 let div_hora = document.getElementById('div_hora');
 
 setInterval(()=> {
@@ -9,47 +9,11 @@ setInterval(()=> {
 	var month = date.getMonth();
 	let month_full = '';
 
-	switch(month) {
-		case 0:
-			month_full = 'Jan';
-			break;
-		case 1:
-			month_full = 'Fev';
-			break;
-		case 2:
-			month_full = 'Mar';
-			break;
-		case 3:
-			month_full = 'Abr';
-			break;
-		case 4:
-			month_full = 'Mai';
-			break;
-		case 5:
-			month_full = 'Jun';
-			break;
-		case 6:
-			month_full = 'jul';
-			break;
-		case 7:
-			month_full = 'ago';
-			break;
-		case 8:
-			month_full = 'Set';
-			break;
-		case 9:
-			month_full = 'Out';
-			break;
-		case 10:
-			month_full = 'Nov';
-			break;
-		case 11:
-			month_full = 'Dez';
-			break
-		default:
-			month_full = 'Mês indefinido';
-			break
+	for(let i in months) {
+		if(month == i) {
+			month_full = months[i];
+		}
 	}
 	
 	div_hora.innerHTML = `${month_full} ${day}, ${hora}:${min}`;
-}, 100)
+}, 1000)
